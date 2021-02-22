177daa77e0d07bb61da6ad6cf90af861da988827
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-02-22 20:44:13 +0530

    drivers: mtk-smi: Use proper printing apis
    * Warnings should be warning, errors should be errors
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- drivers/misc/mediatek/smi/smi_drv.c
+++ drivers/misc/mediatek/smi/smi_drv.c
@@ -71,7 +71,7 @@ static bool smi_sspm_ipi_register;
 		if (cmdq != 0) \
 			cmdq_util_msg(string, ##args); \
 		else \
-			pr_info(string, ##args); \
+			pr_warn(string, ##args); \
 	} while (0)
 #elif IS_ENABLED(CONFIG_MTK_CMDQ)
 #include <cmdq_helper_ext.h>
@@ -79,15 +79,15 @@ static bool smi_sspm_ipi_register;
 	do { \
 		if (cmdq != 0) \
 			cmdq_core_save_first_dump(string, ##args); \
-		pr_info(string, ##args); \
+		pr_warn(string, ##args); \
 	} while (0)
 #else
-#define SMIWRN(cmdq, string, args...) pr_info(string, ##args)
+#define SMIWRN(cmdq, string, args...) pr_warn(string, ##args)
 #endif
 
 #define SMIERR(string, args...) \
 	do { \
-		pr_notice(string, ##args); \
+		pr_err(string, ##args); \
 		aee_kernel_warning(DEV_NAME, string, ##args); \
 	} while (0)
 
