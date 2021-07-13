80228d712dff175730cc994a9e763b3f20687e2f
techyminati <sinha.aryan03@gmail.com>
2021-07-13 12:45:22 +0000

    drivers/scsi/ : Import from merlin-r-oss
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- drivers/scsi/ufs/ufshcd-crypto.c
+++ drivers/scsi/ufs/ufshcd-crypto.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright 2019 Google LLC
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <linux/keyslot-manager.h>
@@ -398,7 +399,11 @@ int ufshcd_prepare_lrbp_crypto_spec(struct ufs_hba *hba,
 
 	lrbp->crypto_enable = true;
 	lrbp->crypto_key_slot = bc->bc_keyslot;
-	lrbp->data_unit_num = bc->bc_dun[0];
+	if (bc->hie_ext4)
+		lrbp->data_unit_num = (u64)(cmd->cmnd[5] | (cmd->cmnd[4] << 8)
+			| (cmd->cmnd[3] << 16) | (cmd->cmnd[2] << 24));
+	else
+		lrbp->data_unit_num = bc->bc_dun[0];
 
 	return 0;
 }
