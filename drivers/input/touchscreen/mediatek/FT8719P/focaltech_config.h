5353364dfecbae617a714274f5af4cdd536c8bda
Rama Bondan Prakoso <ramanarubp@gmail.com>
2021-08-11 18:15:18 +0000

    mediatek: touchscreen: FT8719P: Disable production test
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- drivers/input/touchscreen/mediatek/FT8719P/focaltech_config.h
+++ drivers/input/touchscreen/mediatek/FT8719P/focaltech_config.h
@@ -167,7 +167,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN                             1
+#define FTS_TEST_EN                             0
 
 /*
  * Proximity sensor
